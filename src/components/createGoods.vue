e47f2d7cf5349a1583d274cb6aab345d9fa0a545
HarunaXD-P <1800012989@pku.edu.cn>
2021-10-26 21:45:57 +0800

    add a createGoods page

---
+++ src/components/createGoods.vue
@@ -0,0 +1,25 @@
+<!-- upload the detail and description of good &save -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+
+<style>
+</style>
