556e6f654ba725afd8cdce7aa3e2d0983e2cf1e4
jasonerrr <1472697464@qq.com>
2021-11-15 11:49:33 +0800

    在发布商品界面加了一个返回按钮

--- src/components/goodsCreate.vue
+++ src/components/goodsCreate.vue
@@ -1,86 +1,91 @@
 <!-- upload a good with title,description,num and otc -->
 <template>
   <div class="createInput">
-      <div class="title">
-          <span id="title">商品标题</span>
-          <span id="inputTitle">
-          <div id="titleTextbox">
-              <el-input v-model="inputTitle" placeholder="请输入内容"></el-input>
-              </div>
-          </span>
-      </div>
-      
-      <div class="description">
-          <span id="description">商品描述</span>
-          <span id="inputDescription">
-          <div id="descriptionTextbox">
-              <el-input v-model="inputDescription" 
-                        type="textarea" 
-                        placeholder="请输入内容" :autosize="{minRows:2,maxRows:5}"></el-input>
-              </div>
-          </span>
-      </div>
-      <div class="goodsNum">
-          <span id="goodsNum">选择数量</span>
-          <span id="selectNum">
-              <el-input-number v-model="num" 
-                                @change="handleChange" 
-                                :min="1" :max="10" 
-                                label="描述文字"></el-input-number>
-              
-          </span>
-      </div>
-      <div class="price">
-          <span id="price">选择价格</span>
-          <span id="inputPrice">
-              <div id="priceTextbox">
-              <el-input v-model="inputPrice" 
-                        placeholder="请输入价格"
-                        prefix-icon=""></el-input>
-              </div>
-              
-          </span>
-      </div>
-      <div class="catagory">
-          <span id="catagory">选择分类</span>
-          <span id="selectCatagory">
-              <div id="catagorySelector">
-                   <el-select v-model="value" placeholder="请选择">
-                     <el-option
-                         v-for="item in options"
-                         :key="item.value"
-                        :label="item.label"
-                        :value="item.value">
-                     </el-option>
-                    </el-select>
-              </div>
-              
-          </span>
-      </div>
-      <div class="confirmButton">
-          <div id="button">
-              <el-button type="success" @click="printImg">确认发布</el-button>
+    <div class="title">
+      <span id="title">商品标题</span>
+      <span id="inputTitle">
+        <div id="titleTextbox">
+          <el-input v-model="inputTitle" placeholder="请输入内容"></el-input>
+        </div>
+      </span>
+    </div>
 
+    <div class="description">
+      <span id="description">商品描述</span>
+      <span id="inputDescription">
+        <div id="descriptionTextbox">
+          <el-input
+            v-model="inputDescription"
+            type="textarea"
+            placeholder="请输入内容"
+            :autosize="{ minRows: 2, maxRows: 5 }"
+          ></el-input>
+        </div>
+      </span>
+    </div>
+    <div class="goodsNum">
+      <span id="goodsNum">选择数量</span>
+      <span id="selectNum">
+        <el-input-number
+          v-model="num"
+          @change="handleChange"
+          :min="1"
+          :max="10"
+          label="描述文字"
+        ></el-input-number>
+      </span>
+    </div>
+    <div class="price">
+      <span id="price">选择价格</span>
+      <span id="inputPrice">
+        <div id="priceTextbox">
+          <el-input
+            v-model="inputPrice"
+            placeholder="请输入价格"
+            prefix-icon=""
+          ></el-input>
+        </div>
+      </span>
+    </div>
+    <div class="catagory">
+      <span id="catagory">选择分类</span>
+      <span id="selectCatagory">
+        <div id="catagorySelector">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+      </span>
+    </div>
+    <div class="confirmButton">
+      <div id="button">
+        <el-button type="success" @click="printImg">确认发布</el-button>
+        <el-button type="success" @click="gotoHome">返回</el-button>
+      </div>
+    </div>
+    <div class="picture">
+      <div id="picUploader">
+        <el-upload
+          class="upload-demo"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :file-list="fileList"
+          list-type="picture"
+        >
+          <el-button size="medium" type="primary">点击上传图片</el-button>
+          <div slot="tip" class="el-upload__tip">
+            只能上传jpg/png文件，且不超过500kb
           </div>
-
+        </el-upload>
       </div>
-      <div class="picture">
-          <div id="picUploader">
-              <el-upload
-                class="upload-demo"
-                action="https://jsonplaceholder.typicode.com/posts/"
-                :on-preview="handlePreview"
-                :on-remove="handleRemove"
-                :file-list="fileList"
-                list-type="picture">
-              <el-button size="medium" type="primary">点击上传图片</el-button>
-              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-              </el-upload>
-
-          </div>
-
-      </div>
-      
+    </div>
   </div>
 </template>
 
@@ -104,7 +109,10 @@ export default {
         value: ''
 
     };
+    
+
   },
+
 
   components: {},
 
@@ -122,103 +130,113 @@ export default {
           console.log(this.inputPrice);
           console.log(this.num);
           console.log(this.value);
-      }
+      },
+      gotoHome(){
+        this.$router.replace('/')
+        this.$router.go(0)
+    },
   }
 }
 
 </script>
 
 <style>
-.createInput{
-    float:right;
-    position: relative;
-    width:1300px;
-    height:670px;
-    background:#DEB887;
+.createInput {
+  float: right;
+  position: relative;
+  width: 1300px;
+  height: 670px;
+  background: #deb887;
 
-    margin-top: 15px;
-    
-    border-top-style: solid;
-    border-top-color: darkgray;
-    border-top-width: 2px;
+  margin-top: 15px;
+
+  border-top-style: solid;
+  border-top-color: darkgray;
+  border-top-width: 2px;
 }
-#titleTextbox,#descriptionTextbox{
-    margin-left: 50px;
-    width:400px;
+#titleTextbox,
+#descriptionTextbox {
+  margin-left: 50px;
+  width: 400px;
 }
-#selectNum,#priceTextbox,#catagorySelector{
-    margin-left: 50px;
+#selectNum,
+#priceTextbox,
+#catagorySelector {
+  margin-left: 50px;
 }
 
-.title{
-    height:100px;
-    width:670px;
-    position: absolute;
-    top:50px;
-    left:50px;
+.title {
+  height: 100px;
+  width: 670px;
+  position: absolute;
+  top: 50px;
+  left: 50px;
 }
-.description{
-    height:100px;
-    width:670px;
-    position: absolute;
-    top:150px;
-    left:50px;
+.description {
+  height: 100px;
+  width: 670px;
+  position: absolute;
+  top: 150px;
+  left: 50px;
+}
+.goodsNum {
+  height: 100px;
+  width: 670px;
+  position: absolute;
+  top: 250px;
+  left: 50px;
+}
+.price {
+  height: 100px;
+  width: 670px;
+  position: absolute;
+  top: 350px;
+  left: 50px;
+}
+.catagory {
+  height: 100px;
+  width: 670px;
+  position: absolute;
+  top: 450px;
+  left: 50px;
+}
+.confirmButton {
+  height: 100px;
+  width: 670px;
+  position: absolute;
+  top: 550px;
+  left: 50px;
+}
+.picture {
+  height: 300px;
+  width: 300px;
 
+  position: absolute;
+  left: 700px;
+  top: 160px;
 }
-.goodsNum{
-    height:100px;
-    width:670px;
-    position: absolute;
-    top:250px;
-    left:50px;
+#title,
+#inputTitle {
+  float: left;
+  margin: auto;
 }
-.price{
-    height:100px;
-    width:670px;
-    position: absolute;
-    top:350px;
-    left:50px;
+#description,
+#inputDescription {
+  float: left;
 }
-.catagory{
-    height:100px;
-    width:670px;
-    position: absolute;
-    top:450px;
-    left:50px;
+#goodsNum,
+#selectNum {
+  float: left;
 }
-.confirmButton{
-    height:100px;
-    width:670px;
-    position: absolute;
-    top:550px;
-    left:50px;
+#price,
+#inputPrice {
+  float: left;
 }
-.picture{
-    height:300px;
-    width:300px;
-    
-    position:absolute;
-    left:700px;
-    top:160px;
+#catagory,
+#selectCatagory {
+  float: left;
 }
-#title,#inputTitle{
-    float:left;
-    margin:auto;
-
-}
-#description,#inputDescription{
-    float:left;
-}
-#goodsNum,#selectNum{
-    float:left;
-}
-#price,#inputPrice{
-    float:left;
-}
-#catagory,#selectCatagory{
-    float:left;
-}
-#button{
-    float: center;
+#button {
+  float: center;
 }
 </style>
