c496ac0c7313ceab61acbda2712c7df00244c656
HarunaXD-P <1800012989@pku.edu.cn>
2021-11-24 00:12:48 +0800

    add a page

---
+++ src/page/showMyGoods.vue
@@ -0,0 +1,60 @@
+<!--展示我所发布的商品-->
+<template>
+  <div id="app" style="background-color: #ffffff;">
+    <!--整个显示出来的是一个面板，看效果还很不好，之后要改-->
+    <router-view/>
+    <el-container class="panal">
+      <!--header为上半部分，放了myHeader.vue中的组件-->
+      <el-header>
+        <myHeader> </myHeader>
+      </el-header>
+      <!--main为下半部分，放了LeftSidebar.vue和DisplaySix.vue和myInformation.vue中的三个组件-->
+      <el-main>
+        <!--加一个el-container是为了让这三个组件能左中右排布-->
+        <el-container>
+          <myLeftSidebar></myLeftSidebar>
+          <div id='main'>
+            占个位置
+          </div>
+          <myInformation></myInformation>
+        </el-container>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/myHeader";
+import LeftSidebar from "@/components/LeftSidebar";
+import DisplaySix from "@/components/DisplaySix";
+import information from "@/components/myInformation";
+import "element-ui/lib/theme-chalk/index.css";
+export default {
+  name: "App",
+  components: {
+    myHeader: Header,
+    myLeftSidebar: LeftSidebar,
+    myDisplay: DisplaySix,
+    myInformation: information,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-right : 0px;
+  width:1920px;
+  height:1080px;
+  float:center;
+  margin:auto;
+}
+#main{
+  margin-right: 400px;
+}
+</style>
