5139bce3ea5f582a677aa99099cb38a2c11c7649
HarunaXD-P <75299222+HarunaXD-P@users.noreply.github.com>
2021-11-24 13:36:05 +0800

    add 2 components

---
+++ src/components/displayLong.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div id="longbar">
+        <div id="content">
+          <div style="font-size:40px">{{product_name}}</div>
+          <div>
+            {{product_description}}
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      product_name:"加载中",
+      product_description:"加载中"
+    }
+  },
+  methods:{
+
+  },
+  created(){
+    console.log(this.product_name);
+
+  }
+
+  
+  
+
+}
+</script>
+
+<style>
+#longbar{
+    width:1300px;
+    height:150px;
+    background-color:salmon;
+    margin:auto;
+    margin-bottom: 30px;
+}
+#content{
+  width:700px;
+  height:100px;
+}
+
+</style>
---
+++ src/components/myGoods.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+      wos zhanweifu n
+      <ul>
+          <li v-for="(item,index) in myGoodsData" :key="index">
+                <div id="longbar">
+                    <div id="content">
+                        <div style="font-size:40px">{{item.name}}</div>
+                        <div>
+                            {{item.descript}}
+                        </div>
+                    </div>
+                </div>
+                
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import displayLong from './displayLong.vue'
+export default {
+    components:{displayLong},
+    data(){
+        return{
+            goodsnum:3,
+            //displayLongData:[{'name':"FC31",'descript':"haisidai"},{'name':"FC31",'descript':"wudaiji"},{'name':"F22","descript":'niangniang'}],
+
+        }
+    },
+    created(){
+        console.log("initil");
+        console.log(this.myGoodsData);
+        console.log(this.myGoodsData[1]);
+
+    },
+    methods:{
+
+    },
+    props:['myGoodsData']
+
+}
+</script>
+
+<style>
+ul{
+    list-style: none;
+}
+#longbar{
+    width:1300px;
+    height:150px;
+    background-color:salmon;
+    margin:auto;
+    margin-bottom: 30px;
+}
+#content{
+  width:700px;
+  height:100px;
+}
+</style>
